cccbcd3789893ccfdfc2a3465768485a8ba718ad
Yura Shaplavsky <yurikorock777@gmail.com>
2025-03-28 19:02:38 +0200

    add styles to task 1

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,16 +1,17 @@
+import clsx from "clsx";
 import css from "./Profile.module.css";
 
 export default function Profile({ name, tag, location, image, stats }) {
   return (
-    <div>
-      <div>
-        <img src={image} alt="User avatar" />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+    <div className={css.card}>
+      <div className={css.descr}>
+        <img className={css.image} src={image} alt="User avatar" />
+        <p className={css.name}>{name}</p>
+        <p className={css.username}>@{tag}</p>
+        <p className={css.location}>{location}</p>
       </div>
 
-      <ul>
+      <ul className={css.stats}>
         <li>
           <span>Followers</span>
           <span>{stats.followers}</span>
