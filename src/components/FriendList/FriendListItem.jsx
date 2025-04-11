b50d98428d5489701da615c39feec77a864e34b7
Yura Shaplavsky <yurikorock777@gmail.com>
2025-04-11 16:00:38 +0300

    add styles to status friends

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,11 +1,14 @@
 //FriendListItem.jsx
+import css from "./FriendListItem.module.css";
 
 export default function FriendListItem({ avatar, name, isOnline }) {
   return (
-    <div>
+    <div className={css.friendlistitem}>
       <img src={avatar} alt="Avatar" width="48" />
-      <p>{name}</p>
-      <p>{isOnline ? "Online" : "Offline"}</p>
+      <p className={css.friendName}>{name}</p>
+      <p className={`${css.status} ${isOnline ? css.online : css.offline}`}>
+        {isOnline ? "Online" : "Offline"}
+      </p>
     </div>
   );
 }
